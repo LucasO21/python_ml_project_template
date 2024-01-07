b5a9c53cec2fe34731992c0628b4a0a533a6b0c2
LucasO21 <clfo2014@gmail.com>
2024-01-07 17:18:23 -0500

    logging & exception

---
+++ src/exception.py
@@ -0,0 +1,41 @@
+# ==============================================================================
+# EXCEPTIONS -----
+# ==============================================================================
+
+import sys
+import logging
+
+def error_message_detail(error, error_detail:sys):
+
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = "Error occured in python script name [{0}], line number [{1}], with error message [{2}].format()"
+    file_name, exc_tb.tb_lineno, str(error)
+
+    return error_message
+
+
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error = error_message, error_detail = error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+# if __name__ == '__main__':
+# logging.info("This is a test info message")
+# logging.warning("This is a test warning message")
+# logging.error("This is a test error message")
+# logging.critical("This is a test critical message")
+
+    # try:
+    #     a = 1/10
+    # except Exception as e:
+    #     logging.exception("Error in divide by zero")
+    #     raise CustomException(e, sys)
---
+++ src/logger.py
@@ -0,0 +1,21 @@
+# ==============================================================================
+# LOGGER ----
+# ==============================================================================
+
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f'{datetime.now().strftime("%m_%d_%Y-%H-%M_%S")}.log'
+logs_path = os.path.join(os.getcwd(), 'logs', LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    level    = logging.INFO,
+    format   = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+)
+
+
